f68a50c1b20c4a25c441192fcfc8269c6b6fbb1a
Dev-Luigy <85412506+Dev-Luigy@users.noreply.github.com>
2024-01-20 10:35:17 -0300

    Implementation of Service Label

--- src/main/kotlin/me/dio/credit/applicationsystem/repository/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/applicationsystem/repository/CreditRepository.kt
@@ -2,9 +2,14 @@ package me.dio.credit.applicationsystem.repository
 
 import me.dio.credit.applicationsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: UUID) : Credit?
 
+    @Query(value = "SELECT * FROM CREDIT WHERE customer_id = ?1", nativeQuery = true)
+    fun findAllByCustomer(customerId: Long): List<Credit>
 }
--- src/main/kotlin/me/dio/credit/applicationsystem/service/ICostumerService.kt
+++ src/main/kotlin/me/dio/credit/applicationsystem/service/ICostumerService.kt
@@ -5,5 +5,5 @@ import me.dio.credit.applicationsystem.entity.Customer
 interface ICostumerService {
     fun save(costumer: Customer) : Customer;
     fun findById(id : Long) : Customer;
-    fun delete(id : Long) : Customer;
+    fun delete(id : Long) ;
 }
